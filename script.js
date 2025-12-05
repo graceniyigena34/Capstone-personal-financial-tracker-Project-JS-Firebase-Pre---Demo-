2ffd45b019facd1dd926090ce23b7ec0d6697a4d
graceniyigena34 <graceniyigena34@gmail.com>
2025-12-05 10:00:30 +0200

    added the responsiveness

---
+++ script.js
@@ -0,0 +1,23 @@
+//   // Import the functions you need from the SDKs you need
+//   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+//   // Your web app's Firebase configuration
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyAc6ueFFbfkmrPcnO3LA6X1_GHOQx1olPc",
+//     authDomain: "capstone-2-fe382.firebaseapp.com",
+//     projectId: "capstone-2-fe382",
+//     storageBucket: "capstone-2-fe382.firebasestorage.app",
+//     messagingSenderId: "124019599126",
+//     appId: "1:124019599126:web:c960a45976734ca52f1237"
+//   };
+
+// //   // Initialize Firebase
+// //   const app = initializeApp(firebaseConfig);
+
+// //   //inputs
+// //  const email = document.getElementById('email').Value;
+// //  const password = document.getElementById('password').value;
+// //  const login = document.getElementById('login').value;
+  
